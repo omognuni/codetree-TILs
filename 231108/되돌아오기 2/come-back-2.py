37dc9c556e1b72e1a7e6b5bc3df08a12abb7dcf8
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-08 10:43:40 +0900

    Title: 되돌아오기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231108/되돌아오기 2/come-back-2.py
@@ -0,0 +1,27 @@
+cmd = input()
+x, y = 0, 0
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+dir_num = 0
+count = 0
+
+for c in cmd:
+    count += 1
+    if c == 'F':
+        x += dxs[dir_num]
+        y += dys[dir_num]
+
+        if x == 0 and y == 0:
+            break
+            
+    if c == 'L':
+        dir_num = (3 - dir_num) % 4
+
+    if c == 'R':
+        dir_num = (dir_num + 1) % 4
+    
+
+
+if x != 0 and y != 0:
+    print(-1)
+else:
+    print(count)
