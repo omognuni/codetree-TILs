60fbc214f9c8534916d35c6277ae52986a0be208
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-09 21:32:14 +0900

    Title: 거울에 레이저 쏘기 2, Time: 192ms, Memory: 47MB, Status: Passed - CodeTree

--- 231109/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 231109/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -10,7 +10,7 @@ def in_range(x, y):
     return False
 
 count = 0
-dxs, dys = [1, 0 , -1 ,0], [0, 1, 0, -1]
+dxs, dys = [0, -1 , 0 ,1], [1, 0, -1, 0]
 
 x = 0
 y = 0
@@ -33,17 +33,15 @@ elif K > 3*N and K <= 4*N:
     y = 0
     x = 4*N - K
 
-count = 0
+count = 1
 while True:
-    v = dxs[dir_num] + dys[dir_num]
     if arr[x][y] == '\\':
-        v = v * -1
-
-    if v < 0:
-        dir_num = (dir_num - 1) % 4
+        dir_num = (3 - dir_num) % 4
     else:
-        dir_num = (dir_num + 1) % 4
-
+        if dir_num % 2 == 1:
+            dir_num = dir_num - 1
+        else:
+            dir_num = (dir_num + 1) % 4
     nx = x + dxs[dir_num]
     ny = y + dys[dir_num]
 
@@ -54,4 +52,4 @@ while True:
     y = ny
     count += 1
 
-print(count-1)
+print(count)
