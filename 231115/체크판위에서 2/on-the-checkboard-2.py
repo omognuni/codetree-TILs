4c5a869984da95d14f9c5e18cebd8c3c87b832b8
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-15 21:36:33 +0900

    Title: 체크판위에서 2, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/체크판위에서 2/on-the-checkboard-2.py
+++ 231115/체크판위에서 2/on-the-checkboard-2.py
@@ -10,12 +10,14 @@ suc = 0
 for i in range(R-1):
     for j in range(C-1):
         count = 0
+        x, y = 0, 0
         for k in range(i+1, R):
             for l in range(j+1, C):
                 if arr[i][j] != arr[k][l]:
                     count += 1
+                    x, y = k, l
 
-        if count == 2:
+        if count == 2 and x == R - 1 and y == C - 1:
             suc += 1
 
 print(suc)
