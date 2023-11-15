2876ca10b3c1e56de1ebdcd63cc51e443515bb20
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-15 22:06:51 +0900

    Title: 체크판위에서 2, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 231115/체크판위에서 2/on-the-checkboard-2.py
+++ 231115/체크판위에서 2/on-the-checkboard-2.py
@@ -4,7 +4,7 @@ arr = []
 for _ in range(R):
     arr.append(list(map(str, input().split())))
 
-visited = [[0 for _ in range(R)] for _ in range(C)]
+visited = [[0 for _ in range(C)] for _ in range(R)]
 
 starts = [[0, 0]]
 
@@ -15,11 +15,11 @@ while starts:
     y = start[1]
     for i in range(x+1, R):
         for j in range(y+1, C):
-            if arr[x][y] != arr[i][j] and i < R-1 and j < C-1:
+            if arr[x][y] != arr[i][j] and i != R-1 and j != C-1:
                 visited[i][j] = visited[x][y] + 1
                 starts.append([i, j])
             
-            if i == R-1 and j == C-1 and visited[x][y] == 2:
+            if i == R-1 and j == C-1 and visited[x][y] == 2 and arr[x][y] != arr[i][j]:
                 ans += 1
 
 print(ans)
