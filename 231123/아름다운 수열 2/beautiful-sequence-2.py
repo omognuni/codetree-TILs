bc05194b7f94ab9b0bdd776ba616bb7d5749edbd
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-23 21:26:37 +0900

    Title: 아름다운 수열 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/아름다운 수열 2/beautiful-sequence-2.py
+++ 231123/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,18 +1,20 @@
+# from copy import deepcopy
 N, M = map(int, input().split())
 
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-
 count = 0
 for i in range(N-M+1):
     new_arr = a[i:i+M]
-    flag = True
+    b_visited = [False for _ in range(M)]
+
     for j in range(M):
-        if not b[j] in new_arr:
-            flag = False
-            break
-    if flag:
+        for k in range(M):
+            if b[k] == new_arr[j] and not b_visited[k]:
+                b_visited[k]=True
+
+    if False not in b_visited:
         count+=1
 
 print(count)
