6f38a73720af154fdac1b840b7372f3dd54efcd1
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-30 21:45:46 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -9,22 +9,22 @@ arrays = []
 for i in range(a1-2, a1+3):
     for j in range(b1-2, b1+3):
         for k in range(c1-2, c1+3):
-            if i <= 0 or j <= 0 or k <= 0:
+            if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
+                d, e, f = numbers[i], numbers[j], numbers[k]
+                if [d, e, f] not in arrays:
+                    arrays.append([d, e, f])
+            else:
                 continue
-            elif i not in numbers or j not in numbers or k not in numbers:
-                continue
-            elif [i, j, k] not in arrays:
-                arrays.append([i, j, k])
 
 
 for i in range(a2-2, a2+3):
     for j in range(b2-2, b2+3):
         for k in range(c2-2, c2+3):
-            if i <= 0 or j <= 0 or k <= 0:
+            if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
+                d, e, f = numbers[i], numbers[j], numbers[k]
+                if [d, e, f] not in arrays:
+                    arrays.append([d, e, f])
+            else:
                 continue
-            elif i not in numbers or j not in numbers or k not in numbers:
-                continue
-            elif [i, j, k] not in arrays:
-                arrays.append([i, j, k])
 
 print(len(arrays))
