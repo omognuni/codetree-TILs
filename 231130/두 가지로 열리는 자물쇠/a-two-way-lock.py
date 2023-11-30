1c69f57f4967071abcff29c9dcf8806846a7328f
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-30 21:49:40 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -6,9 +6,9 @@ numbers = [i for i in range(1, N+1)]
 
 arrays = []
 
-for i in range(a1-2, a1+3):
-    for j in range(b1-2, b1+3):
-        for k in range(c1-2, c1+3):
+for i in range(a1-3, a1+2):
+    for j in range(b1-3, b1+2):
+        for k in range(c1-3, c1+2):
             if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
                 d, e, f = numbers[i], numbers[j], numbers[k]
                 if [d, e, f] not in arrays:
@@ -17,9 +17,9 @@ for i in range(a1-2, a1+3):
                 continue
 
 
-for i in range(a2-2, a2+3):
-    for j in range(b2-2, b2+3):
-        for k in range(c2-2, c2+3):
+for i in range(a2-3, a2+2):
+    for j in range(b2-3, b2+2):
+        for k in range(c2-3, c2+2):
             if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
                 d, e, f = numbers[i], numbers[j], numbers[k]
                 if [d, e, f] not in arrays:
