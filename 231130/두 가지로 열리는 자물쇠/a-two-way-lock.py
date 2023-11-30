ae8fe83488e0b0c97883d62c0a32ae6826068acc
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-30 21:35:08 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,22 @@
+N = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+numbers = [i for i in range(1, N+1)]
+
+arrays = []
+
+for i in range(a1-2, a1+3):
+    for j in range(b1-2, b1+3):
+        for k in range(c1-2, c1+3):
+            if [i, j, k] not in arrays:
+                arrays.append([i, j, k])
+
+
+for i in range(a2-2, a2+3):
+    for j in range(b2-2, b2+3):
+        for k in range(c2-2, c2+3):
+            if [i, j, k] not in arrays:
+                arrays.append([i, j, k])
+
+print(len(arrays))
