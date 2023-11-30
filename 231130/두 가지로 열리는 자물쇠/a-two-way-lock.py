ea905173771e2d0bb64d9766c3931b708870d700
yongun <66303652+omognuni@users.noreply.github.com>
2023-11-30 21:56:26 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 231130/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -9,22 +9,19 @@ arrays = []
 for i in range(a1-3, a1+2):
     for j in range(b1-3, b1+2):
         for k in range(c1-3, c1+2):
-            if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
-                d, e, f = numbers[i], numbers[j], numbers[k]
-                if [d, e, f] not in arrays:
-                    arrays.append([d, e, f])
-            else:
-                continue
+            i, j, k = i % N, j % N, k % N
+            d, e, f = numbers[i], numbers[j], numbers[k]
+            if [d, e, f] not in arrays:
+                arrays.append([d, e, f])
 
 
 for i in range(a2-3, a2+2):
     for j in range(b2-3, b2+2):
         for k in range(c2-3, c2+2):
-            if i < N and i >= -N and j < N and j >= -N and k < N and k >= -N:
-                d, e, f = numbers[i], numbers[j], numbers[k]
-                if [d, e, f] not in arrays:
-                    arrays.append([d, e, f])
-            else:
-                continue
+            i, j, k = i % N, j % N, k % N
+            d, e, f = numbers[i], numbers[j], numbers[k]
+            if [d, e, f] not in arrays:
+                arrays.append([d, e, f])
+
 
 print(len(arrays))
